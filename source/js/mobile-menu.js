c3471ee9db014e4d43ad8514dc8a5ae6992719d5
Михаил <kastiel85@mail.ru>
2021-08-29 13:51:09 +0300

    29.08.2021

---
+++ source/js/mobile-menu.js
@@ -0,0 +1,15 @@
+var nav = document.querySelector(".page-header__nav");
+var navBtnToggle = document.querySelector(".page-header__logo-toggle");
+
+nav.classList.remove("page-header__nav--nojs");
+
+navBtnToggle.addEventListener("click", function(evt) {
+    // evt.preventDefault ();
+    if (nav.classList.contains("page-header__nav--closed")) {
+        nav.classList.remove("page-header__nav--closed");
+        nav.classList.add("page-header__nav--opened");
+    } else {
+        nav.classList.remove("page-header__nav--opened");
+        nav.classList.add("page-header__nav--closed");
+    }
+});
