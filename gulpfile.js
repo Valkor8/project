ee303ee21cbaf7d00caa7b1600a2afbc285f127e
Михаил <kastiel85@mail.ru>
2021-09-27 22:21:15 +0300

    27.09.2021

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,13 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require('gulp-sass');
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp")
 const sync = require("browser-sync").create();
 
-// CSS
+// Styles
 
 const styles = () => {
     return gulp.src("source/sass/style.scss")
@@ -16,14 +20,40 @@ const styles = () => {
         .pipe(sourcemap.init())
         .pipe(sass())
         .pipe(postcss([
-            autoprefixer()
+            autoprefixer(),
+            csso()
         ]))
         .pipe(sourcemap.write("."))
+        .pipe(rename("style.min.css"))
         .pipe(gulp.dest("source/css"))
         .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Images
+
+const images = () => {
+    return gulp.src("source/img/**/*.{jpg,png,svg}")
+        .pipe(imagemin([
+            imagemin.mozjpeg({progressive: true}),
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest("dist/img"));
+}
+
+exports.images = images;
+
+// Webp
+
+const createWebp = () => {
+    return gulp.src("source/img/**/*.{jpg,png}")
+        .pipe(webp({quality: 90}))
+        .pipe(gulp.dest("source/img"))
+}
+
+exports.createWebp = createWebp;
 
 //  Сервер
 
